9e22fb63c6d59162ac6c0bbd50c8b42a34ec6fa6
jCodeWizz <91249470+jCodeWizz@users.noreply.github.com>
2022-10-05 15:20:15 +0200

    e

--- libGDX/core/src/dev/codewizz/main/Main.java
+++ libGDX/core/src/dev/codewizz/main/Main.java
@@ -1,6 +1,8 @@
 package dev.codewizz.main;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import java.util.Iterator;
+
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.audio.Music;
@@ -15,7 +17,7 @@ import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.TimeUtils;
 
-public class Main extends ApplicationAdapter {
+public class Main extends Game {
 
 	private Texture dropImage;
 	private Array<Rectangle> raindrops;
@@ -52,6 +54,10 @@ public class Main extends ApplicationAdapter {
 		bucket.y = 20;
 		bucket.width = 64;
 		bucket.height = 64;
+		
+		raindrops = new Array<Rectangle>();
+		spawnRaindrop();
+
 	}
 
 	@Override
@@ -63,6 +69,9 @@ public class Main extends ApplicationAdapter {
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 		batch.draw(bucketImage, bucket.x, bucket.y);
+		for(Rectangle raindrop: raindrops) {
+			batch.draw(dropImage, raindrop.x, raindrop.y);
+		}
 		batch.end();
 
 		// TOUCH AND CLICK INPUT
@@ -77,8 +86,24 @@ public class Main extends ApplicationAdapter {
 			bucket.x -= 200 * Gdx.graphics.getDeltaTime();
 		
 		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) 
-			bucket.x += 200 * Gdx.graphics.getDeltaTime();
+			bucket.x += 200 * Gdx.graphics.getDeltaTime();   
+		
+		if(bucket.x < 0) bucket.x = 0;
+		if(bucket.x > 800 - 64) bucket.x = 800 - 64;
 
+		if(TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnRaindrop();
+		
+		for (Iterator<Rectangle> iter = raindrops.iterator(); iter.hasNext();) {
+			Rectangle raindrop = iter.next();
+			raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
+			if (raindrop.y + 64 < 0)
+				iter.remove();
+			
+			if(raindrop.overlaps(bucket)) {
+				dropSound.play();
+				iter.remove();
+		    }
+		}
 	}
 	
 	private void spawnRaindrop() {
@@ -93,6 +118,7 @@ public class Main extends ApplicationAdapter {
 	
 	@Override
 	public void dispose () {
-
+		dropImage.dispose();
+		bucketImage.dispose();
 	}
 }
