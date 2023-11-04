12b93cd53d6a1d2fe20c64c29b892d44f99949c3
CodeWizz <tom.heijkamp@outlook.com>
2023-11-04 17:04:21 +0100

    sdf

--- Test/src/dev/codewizz/main/Main.java
+++ Test/src/dev/codewizz/main/Main.java
@@ -1,21 +1,19 @@
 package dev.codewizz.main;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
-
-		for(int i = -64; i < 64; i++) {
-			System.out.println(i + " - " + roundHalfUp(i, 16));
+		
+		List<Integer> list = Arrays.asList(3, 4, 5, 7, 8);
+		
+		for(Integer i : list) {
+			System.out.println(i);
 		}
 		
+		list.add(8);
 		
-		
-		
-		
-		
-	}
-	
-	public static int roundHalfUp(int value, int multiplier) {
-	    return (value + (value < 0 ? multiplier / -2 : multiplier / 2)) / multiplier * multiplier;
 	}
 }
